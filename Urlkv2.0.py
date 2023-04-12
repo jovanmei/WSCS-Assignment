d68a61b6f02f99ca47639d29880bc3b66faae73c
TLinz <linz1114@163.com>
2023-04-13 00:22:11 +0200

    impr hashing(add timestamp)

---
+++ Urlkv2.0.py
@@ -0,0 +1,117 @@
+import hashlib
+import time
+import validators
+from flask import Flask, request, jsonify
+import redis
+
+# Create Flask application
+app = Flask(__name__)
+# Configure Redis connection
+app.config['REDIS_URL'] = 'redis://localhost:6379/0'
+# Connect to Redis database
+db = redis.Redis.from_url(app.config['REDIS_URL'])
+
+# URL class to store URL value and its hash
+class Url:
+    def __init__(self, value):
+        self.value = value
+        self.hash = self.hash_value()
+
+    # Generate a unique hash based on the URL value and current timestamp
+    def hash_value(self):
+        hasher = hashlib.sha256()
+        hasher.update(self.value.encode('utf-8'))
+        timestamp = str(int(time.time()))[-5:]
+        short_hash = hasher.hexdigest()[:4]
+        return f"{timestamp}-{short_hash}"
+
+# Check if the given URL is already in the database, return its hash if found
+def get_existing_hash(url_value):
+    for key in db.scan_iter():
+        if db.get(key).decode('utf-8') == url_value:
+            return key.decode('utf-8')
+    return None
+
+# Create a new short URL
+@app.route('/', methods=['POST'])
+def create():
+    value = request.json['value']
+
+    # Validate the URL
+    if validators.url(value):
+        # Check if the URL already exists in the database
+        existing_hash = get_existing_hash(value)
+        if existing_hash:
+            # Return the existing hash with a warning message
+            return jsonify({'value': existing_hash, 'warning': 'URL already exists in the database'}), 200
+
+        # Create a new URL object and store it in the database
+        url = Url(value=value)
+        db.set(url.hash, url.value)
+
+        # Return the new hash
+        return jsonify({'value': url.hash}), 201
+    else:
+        # Invalid URL format
+        return jsonify({'error': 'Invalid URL format'}), 400
+
+# Get all short URLs
+@app.route('/', methods=['GET'])
+def get_all():
+    output = []
+    # Iterate through the database keys and append the URL values and hashes to the output
+    for key in db.scan_iter():
+        output.append({'value': db.get(key).decode('utf-8'), 'hash': key.decode('utf-8')})
+    return jsonify({'urls': output}), 200
+
+# Get the original URL for a short URL
+@app.route('/<hash>', methods=['GET'])
+def get(hash):
+    value = db.get(hash)
+    if not value:
+        # URL not found
+        return jsonify({'message': 'URL not found'}), 404
+    # Return the original URL and hash
+    return jsonify({'value': value.decode('utf-8'), 'hash': hash}), 200
+
+# Update a short URL with a new URL
+@app.route('/<hash>', methods=['PUT'])
+def update(hash):
+    value = db.get(hash)
+    if not value:
+        # URL not found
+        return jsonify({'message': 'URL not found'}), 404
+    new_value = request.json['value']
+    # Validate the new URL
+    if validators.url(new_value):
+        # Create a new hash for the updated URL
+        new_hash = Url(value=new_value).hash
+        # Remove the old hash from the database and add the new one
+        db.delete(hash)
+        db.set(new_hash, new_value)
+        return jsonify({'value': new_value, 'hash': new_hash})
+    else:
+        # Invalid URL format
+        return jsonify({'error': 'Invalid URL format'}), 400
+
+# Delete a short URL
+@app.route('/<hash>', methods=['DELETE'])
+def delete(hash):
+    value = db.get(hash)
+    if not value:
+        # URL not found
+        return jsonify({'message': 'URL not found'}), 404
+    # Delete the URL from the database
+    db.delete(hash)
+    return '', 204
+
+# Delete all short URLs
+@app.route('/', methods=['DELETE'])
+def delete_all():
+    # Flush the Redis database
+    db.flushdb()
+    return '', 204
+
+# Run the Flask application
+if __name__ == '__main__':
+    app.run(debug=True)
